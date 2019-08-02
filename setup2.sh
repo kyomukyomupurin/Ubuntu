e2ddda86c371a22b061603c7c13f3acb9e7efb7a
kyomukyomupurin <murakami1728@gmail.com>
2019-08-03 08:18:58 +0900

    initial commit

---
+++ setup2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# git setting
+git config --global user.name "kyomukyomupurin"
+git config --global user.email murakami1728@gmail.com
+
+# install rust
+curl https://sh.rustup.rs -sSf | sh
+echo "export PATH="$HOME/.cargo/bin:$PATH"" >> ~/.bashrc
+
+# install crystal
+curl -sSL https://dist.crystal-lang.org/apt/setup.sh | sudo bash
+yes | sudo apt install crystal
+
+# install nim
+yes | sudo apt install nim
+
+# clean up
+yes | sudo apt autoremove
