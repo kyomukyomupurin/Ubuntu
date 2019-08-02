7f14b8d3f05f5597e4cf36e09dccbd6e72059fe1
kyomukyomupurin <murakami1728@gmail.com>
2019-08-03 05:45:02 +0900

    update

--- install.sh
+++ install.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+# update
 yes | sudo apt update
 yes | sudo apt upgrade
-yes | sudo apt install git curl tree
+
+# install basic commands
+yes | sudo apt install git curl tree build-essential
+
+# install c++ tools
+yes | sudo apt install build-essential
+
+# git setting
 yes | sudo git config --global user.name "kyomukyomupurin"
 yes | sudo git config --global user.email murakami1728@gmail.com
-yes | sudo apt install build-essential
+
+# install Python3
 yes | sudo apt install proj-bin libproj-dev libgeos-dev python3-pip python3-tk
+
+# for cartopy
+yes | sudo apt install proj-bin libproj-dev libgeos-dev
+
+# install Python library
 yes | sudo pip3 install numpy 
 yes | sudo pip3 install scipy
 yes | sudo pip3 install pandas
@@ -19,4 +33,6 @@ yes | sudo pip3 install seaborn
 yes | sudo pip3 install jupyter
 yes | sudo pip3 install jupyterlab
 yes | sudo pip3 uninstall shapely && sudo pip3 install --no-binary :all: shapely
+
+# clean
 yes | sudo apt autoremove
